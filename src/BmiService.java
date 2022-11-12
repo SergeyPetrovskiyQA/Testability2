3cad788a8af9c388dd18a0549d52d9a1605a1d06
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2022-11-12 15:02:11 +0300

    modified

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,11 +1,9 @@
 public class BmiService {
-    public float index (float weight, float growht){
-        float myGrowht = growht/100;
-        float bmi = weight/(myGrowht*myGrowht);
+    public float index(float weight, float growht) {
+        float myGrowht = growht / 100;
+        float bmi = weight / (myGrowht * myGrowht);
         return bmi;
     }
-
-
 
 
 }
